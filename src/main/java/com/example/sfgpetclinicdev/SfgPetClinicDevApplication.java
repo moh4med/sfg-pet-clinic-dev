ceaa4927b81118d742da91d6f2fa11936e8bc802
mohamed <moh4med.ahmed@gmail.com>
2021-10-05 15:10:57 +0200

    init project

---
+++ src/main/java/com/example/sfgpetclinicdev/SfgPetClinicDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.sfgpetclinicdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sfgpetclinicdev/SfgPetClinicDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sfgpetclinicdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
