7d1d41fcbcfc0cc688ccfe5c08ab77c13e816f8b
mohamed <moh4med.ahmed@gmail.com>
2021-10-07 00:18:31 +0200

    refactor map impl to abstract Id generation

--- pet-clinic-data/src/main/java/com/example/sfgpetclinicdev/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinicdev/services/map/AbstractMapService.java
@@ -1,20 +1,25 @@
 package com.example.sfgpetclinicdev.services.map;
 
+import com.example.sfgpetclinicdev.model.BaseEntity;
 import com.example.sfgpetclinicdev.services.CrudService;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
-public abstract class AbstractMapService<T,ID> implements CrudService<T,ID> {
-    protected Map<ID,T> map=new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity,ID extends Long> implements CrudService<T,ID> {
+    protected Map<Long,T> map=new HashMap<>();
     @Override
     public T findById(ID id) {
         return map.get(id);
     }
-    public T save(ID id,T object) {
-        map.put(id,object);
+    public T save(T object) {
+        if(null!=object){
+            if(object.getId()==null){
+                object.setId(getNextId());
+            }
+            map.put(object.getId(),object);
+        }else{
+            throw new RuntimeException("object can't be null");
+        }
         return object;
     }
     @Override
@@ -30,5 +35,15 @@ public abstract class AbstractMapService<T,ID> implements CrudService<T,ID> {
     @Override
     public void delete(T object) {
         map.entrySet().removeIf(idtEntry -> idtEntry.getValue().equals(object));
+    }
+    private Long getNextId(){
+        Long id=null;
+        try {
+            id = Collections.max(map.keySet())+1;
+        }catch (NoSuchElementException e){
+            id=1L;
+        }
+
+        return id;
     }
 }
--- pet-clinic-data/src/main/java/com/example/sfgpetclinicdev/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinicdev/services/map/PetServiceMap.java
@@ -6,8 +6,5 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PetServiceMap extends AbstractMapService<Pet,Long> implements PetService {
-    @Override
-    public Pet save(Pet object) {
-        return super.save(object.getId(),object);
-    }
+
 }
