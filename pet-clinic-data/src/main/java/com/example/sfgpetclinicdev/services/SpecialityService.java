3b042df9cde72cb85541d83136d96dca8d5d846a
mohamed <moh4med.ahmed@gmail.com>
2021-10-07 22:58:37 +0200

    create speciality service

---
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinicdev/services/SpecialityService.java
@@ -0,0 +1,8 @@
+package com.example.sfgpetclinicdev.services;
+
+import com.example.sfgpetclinicdev.model.Speciality;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface SpecialityService extends CrudService<Speciality,Long>{
+}
---
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinicdev/services/map/SpecialityServiceMap.java
@@ -0,0 +1,10 @@
+package com.example.sfgpetclinicdev.services.map;
+
+import com.example.sfgpetclinicdev.model.Speciality;
+import com.example.sfgpetclinicdev.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality,Long> implements SpecialityService {
+
+}
